a2940915f740b6dd4eef690f59d0909585c6cbdf
YHop <ypeng.tw@gmail.com>
2015-09-29 16:41:21 -0700

    Add date add in rental js

--- app/models/rental.js
+++ app/models/rental.js
@@ -2,9 +2,10 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   owner: DS.attr(),
-  // city: DS.belongsTo('city', {async: true}),
-  city: DS.attr(),
+  // city: DS.attr(),
+  city: DS.belongsTo('city', {async: true}),
   type: DS.attr(),
   image: DS.attr(),
-  bedrooms: DS.attr()
+  bedrooms: DS.attr(),
+  date_added: DS.attr()
 });
