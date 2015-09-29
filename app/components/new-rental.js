454acff7003d2817686be8d7bcc658a2ecbd3a30
YHop <ypeng.tw@gmail.com>
2015-09-29 16:32:37 -0700

    modify saveRental and add update1

--- app/components/new-rental.js
+++ app/components/new-rental.js
@@ -15,9 +15,21 @@ export default Ember.Component.extend({
         type: this.get('type'),
         image: this.get('image'),
         bedrooms: this.get('bedrooms'),
+        date_added: Date.now()
       };
       this.set('addNewRental', false),
       this.sendAction('saveRental', params);
+    },
+
+    save1() {
+      var params = {
+        owner: this.get('owner'),
+        city: this.get('city'),
+        type: this.get('type'),
+        image: this.get('image'),
+        bedrooms: this.get('bedrooms'),
+      };
+      this.sendAction('update2', params);
     }
   }
 });
